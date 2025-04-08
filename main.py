57da1dcfb7cc32ac100e5afc710614a1da4430dc
etopritika <dimaprytyka@gmail.com>
2025-04-08 14:31:31 +0300

    Added missing import Optional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 from ml_engine import recommend_movies_by_favorites
 
 app = FastAPI()
