a11a4fd327dc34e0a6c11a95f7edbff773e07b50
etopritika <dimaprytyka@gmail.com>
2025-04-09 15:31:28 +0300

    feat: enable CORS for movie-radar-rho.vercel.app

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models import MovieRequest
 from ml_engine import recommend_movies_by_favorites
 
 app = FastAPI()
+
+origins = [
+    "https://movie-radar-rho.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/recommendations")
 def recommend(payload: MovieRequest):
